e8205fc8a3f44cebecc6622319a5e0ce5a960c79
mahbubrahman1 <mahbuburrofficial@gmail.com>
2022-04-25 06:13:28 +0600

    added load meal file

--- JSON/api/More-API/countries-name/practice/script.js
+++ JSON/api/More-API/countries-name/practice/script.js
@@ -10,11 +10,15 @@ const showCountries = countries => {
     const countriesBody = document.getElementById('countries');
     countries.forEach(country => {
         // console.log(country);
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name.common;
-        countriesBody.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        countriesBody.appendChild(p);
-    })
+        const div = document.createElement('div');
+        // h3.innerText = country.name.common;
+        // countriesBody.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // countriesBody.appendChild(p);
+        div.innerHTML = `
+        <h3>Name: ${country.name.common}</h3>
+        <p>Capital: ${country.capital}</p>`
+        countriesBody.appendChild(div);
+    });
 }
