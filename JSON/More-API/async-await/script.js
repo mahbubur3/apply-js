a3d7784b64f09d7d378b8b99653893227968acc8
mahbubur3 <mahbubur9@yahoo.com>
2024-02-18 16:37:44 +0600

    updated

--- JSON/More-API/async-await/script.js
+++ JSON/More-API/async-await/script.js
@@ -2,6 +2,7 @@ const loadCountries = async () => {
     const url = 'https://restcountries.com/v3.1/all';
     const response = await fetch(url);
     const data = await response.json();
+
     displayCountries(data);
 };
 
@@ -10,15 +11,21 @@ loadCountries();
 const displayCountries = countries => {
     // console.log(countries);
     const countriesBody = document.getElementById('countries-body');
+
     countries.forEach(country => { // for (const country of countries)
         const div = document.createElement('div');
         div.classList.add('country');
+
         const h3 = document.createElement('h3');
         h3.innerText = country.name.common;
+
         div.appendChild(h3);
+
         const p = document.createElement('p');
         p.innerText = country.capital;
+
         div.appendChild(p);
+
         countriesBody.appendChild(div);
     });
 }
