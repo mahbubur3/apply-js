f86c1b0ab9caae8a63d3f02496f4a7b133e9e03d
mahbubur3 <mahbubur9@yahoo.com>
2024-02-18 12:03:44 +0600

    practice template-string

--- ES6/template-string.js
+++ ES6/template-string.js
@@ -8,6 +8,24 @@
 // let result = x + y;
 // console.log(`The sum of ${x} and ${y} is: ${result}`);
 
-let firstName = 'Mahbubur';
-let lastName = 'Rahman';
-console.log(`Full Name: ${firstName} ${lastName}`);
+// let firstName = 'Mahbubur';
+// let lastName = 'Rahman';
+// console.log(`Full Name: ${firstName} ${lastName}`);
+
+
+
+// const a = 50, b = 70;
+// const result = a + b;
+// console.log(`The sum result ${result}`);
+
+
+// const firstName = 'Mahbubur'
+// const LastName = 'Rahman'
+// console.log(`Full name: ${firstName} ${LastName}`);
+
+
+const num1 = 50;
+const num2 = 90;
+const total = num1 + num1
+
+console.log(`The sum of ${num1} and ${num2} is: ${total}`);
