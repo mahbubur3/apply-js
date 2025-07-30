7dc3bfd3d17337e5e370560e1e71ec9e6ee81858
mahbubur3 <mahbubur7716@gmail.com>
2025-07-30 13:47:38 +0600

    added new folder

---
+++ explore-js-new/test-two.js
@@ -0,0 +1,18 @@
+// const numbers = [9, 2, 4, 7];
+// for (const num of numbers ) {
+//     console.log(num);
+// }
+
+
+// const brands = ["Apple", "Lenovo", "Asus", "Dell"];
+// for (const brand of brands) {
+//     console.log(brand);
+// }
+
+
+// WHILE LOOP
+let num = 1;
+while (num <= 5) {
+    console.log(num);
+    num++;
+}
